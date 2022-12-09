05d37b8f017ac2dbe2f4fa85677b81525e988800
Eric Marquez <emarq@microsoft.com>
2022-12-09 18:16:16 +0000

    updated the second half of the code problem to place all the data into a slice, sort it and get the last items in the slice.

--- src/2022/Day1.2/day1.2.go
+++ src/2022/Day1.2/day1.2.go
@@ -6,70 +6,54 @@ Find the top three Elves carrying the most Calories. How many Calories are those
 package main
 
 import (
-	"fmt"
 	"bufio"
+	"fmt"
 	"os"
-	"strings"
+	"sort"
 	"strconv"
+	"strings"
 )
-/*
-* Sample Data
-1000
-2000
-3000
 
-4000
-
-5000
-6000
-
-7000
-8000
-9000
-
-10000
-*/
 func main() {
+	i := 0     // counter to determine the elf
+	total := 0 // calories the elf is holding, this is rest for each elf
+	var elfTotal = []int{}
+	slc_total := append(elfTotal, 0)
 
-	// this looks like a linked list, creating a stack structure
-	// Last in first out.  this will allow me to capture the top 3 leaders.
-	// If the current elf has more calroies than the current leader,
-	//    push into the stack and repeat. 
-	// If the current elf does not have more calories then ignore the elf.
-	// pop the first three elfs from the linked list and sum them.  This is the answer.
-
-	elf_leader_sum := 0 // sum of leader1
-	elf_leader := 0		// elf leader number
-
-	elf := 0			// counter to determine the elf
-	cal := 0 			// calories the elf is holding, this is rest for each elf
-	
 	readFile, err := os.Open("testdata.txt")
-    if err != nil {
-        fmt.Println(err)
-    }
+	if err != nil {
+		fmt.Println(err)
+	}
 
-    fileScanner := bufio.NewScanner(readFile)
-    fileScanner.Split(bufio.ScanLines)
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
 
-    for fileScanner.Scan() {
-        line_str := strings.TrimSpace(fileScanner.Text())
+	for fileScanner.Scan() {
+		line_str := strings.TrimSpace(fileScanner.Text())
 		item, _ := strconv.Atoi(line_str)
-		cal += item  // sum the elf's total calories
 
 		// identify if this is a new elf, new elf is zero
 		if item == 0 {
-			fmt.Printf("Elf [%d] had [%d] Calories\n",elf,cal)
-			if cal > elf_leader_sum {
-				fmt.Printf(" -- New Leader Elf [%d] --Previous Elf [%d] had [%d] Calories\n", elf,elf_leader,elf_leader_sum)
-				elf_leader_sum = cal
-				elf_leader = elf
-			}
-			elf++	// increment to the next elf
-			cal = 0	// new elf, rest back to zero
+			//fmt.Println("Total:", total)
+			slc_total = append(slc_total, total)
+			//fmt.Println(slc_total)
+			i++       // increment to the next elf
+			total = 0 // new elf, rest back to zero
+		} else {
+			total += item
 		}
-    }
-	fmt.Printf("\nLead EFl [%d] had [%d] calories\n",elf_leader, elf_leader_sum)
+	}
+	readFile.Close()
 
-    readFile.Close()
+	sort.Ints(slc_total)
+	//fmt.Println("length: ", l)
+	l := len(slc_total) - 1
+	end := l - 2
+	var total_cal int
+	for ; l >= end; l--  {
+		fmt.Println(slc_total[l])
+		total_cal += slc_total[l]
+	}
+	fmt.Println(slc_total)
+	fmt.Println("total: ", total_cal)
 }
---
+++ src/2022/Day1.2/go.mod
@@ -0,0 +1,3 @@
+module day1.2
+
+go 1.19
